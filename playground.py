6882d531de62a0c78aac18ad9251359cd8a334fa
gunturajip <gunturaji852@gmail.com>
2024-05-24 15:14:56 +0700

    final phase dev

--- playground.py
+++ playground.py
@@ -6,7 +6,7 @@ import nltk
 import string
 import tensorflow as tf
 import tensorflowjs as tfjs
-from sklearn.metrics import classification_report, confusion_matrix
+from sklearn.model_selection import train_test_split
 
 # stemmer = MPStemmer()
 stopword_list = [i.replace('\n', '') for i in open('stopwords.txt', 'r').readlines()]
@@ -41,10 +41,51 @@ print(len(is_duplicate))
 print("AFTER IS_DUPLICATE IS SET")
 print(len(set(is_duplicate)))
 
-model = tf.keras.models.load_model("selu/selu_nadam.h5")
-tfjs.converters.save_keras_model(model, "selu_nadam_tfjs")
+words = []
+classes = []
+documents = []
 
-y_true = [0, 1, 2, 2, 2, 3]
-y_pred = [0, 0, 2, 2, 1, 3]
-target_names = ['class 0', 'class 1', 'class 2', 'class 3']
-print(classification_report(y_true, y_pred, target_names=target_names))
+for intent in intents['intents']: # PARSING
+    for pattern in intent['patterns']:
+        pattern = pattern.translate(str.maketrans({_: ' ' for _ in string.punctuation})) # SPECIAL CHARACTERS REMOVAL
+        pattern = pattern.lower().strip() # CASE FOLDING
+        # pattern = stemmer.stem_kalimat(pattern) # STEMMING
+        # print("DEBUG STEMMING")
+        # print(pattern)
+        pattern = stopword_kalimat(pattern) # STOPWORDS REMOVAL
+        word_list = nltk.word_tokenize(pattern) # TOKENIZING
+        words.extend(word_list)
+        documents.append((word_list, intent['tag']))
+        if intent['tag'] not in classes:
+            classes.append(intent['tag'])
+
+words = sorted(set(words))
+classes = sorted(set(classes))
+print("JUMLAH DATA PADA VARIABEL words: ", len(words))
+print("JUMLAH DATA PADA VARIABEL classes: ", len(classes))
+print("JUMLAH DATA PADA VARIABEL documents: ", len(documents))
+
+dataset = []
+output_empty = [0] * len(classes)
+for document in documents:
+    bag = []
+    word_patterns = document[0]
+    for word in words:
+        bag.append(1) if word in word_patterns else bag.append(0)
+    output_row = list(output_empty)
+    output_row[classes.index(document[1])] = 1
+    dataset.append([bag, output_row])
+
+random.shuffle(dataset)
+dataset = np.array(dataset)
+
+X = list(dataset[:, 0])
+y = list(dataset[:, 1])
+
+X_train, X_val, y_train, y_val = train_test_split(X, y, test_size=0.2)
+print("PANJANG DATA X: ",len(X))
+print("PANJANG DATA y: ",len(y))
+print("PANJANG DATA X_train: ", len(X_train))
+print("PANJANG DATA X_val: ", len(X_val))
+print("PANJANG DATA y_train: ", len(y_train))
+print("PANJANG DATA y_val: ", len(y_val))
